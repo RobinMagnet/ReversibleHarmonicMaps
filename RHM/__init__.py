aa70d83d38d468493b7679566ac0913424ac4207
robin <robin@ampere.lix.polytechnique.fr>
2024-04-30 20:15:58 +0200

    test doc

--- RHM/__init__.py
+++ RHM/__init__.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-
-DENSEMAPS_PATH = str(Path(__file__).parents[1] / "ScalableDenseMaps")
+import os
+DENSEMAPS_PATH = os.path.abspath(str(Path(__file__).parents[1] / "ScalableDenseMaps"))
 if DENSEMAPS_PATH not in sys.path:
     sys.path.append(DENSEMAPS_PATH)
